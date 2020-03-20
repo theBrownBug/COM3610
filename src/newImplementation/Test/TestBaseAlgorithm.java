09f69cf349f03b2c3f07d3ed144d7d8a31dcc24e
Eeshan Jaiswal <esjaiswal1@sheffield.ac.uk>
2020-03-20 22:23:34 +0000

    Fix one test case

--- src/newImplementation/Test/TestBaseAlgorithm.java
+++ src/newImplementation/Test/TestBaseAlgorithm.java
@@ -10,6 +10,18 @@ import static org.junit.Assert.assertNotEquals;
 
 public class TestBaseAlgorithm {
 
+    /**
+     * Same graph for test case 1 and 2
+     * Graph  for test 1 and test 2 +tive and -tive:
+     *  8 vertices
+     *  6 edges
+     *
+     *  0 --- 1
+     *  2-3-4-5-6-7
+     *
+     * */
+
+    // test 1
     @Test
     public void testOnePositiveBaseAlgo(){
         Graph<Integer , DefaultEdge> sampleGraph = new SimpleGraph<>(DefaultEdge.class);
@@ -32,6 +44,43 @@ public class TestBaseAlgorithm {
             sampleGraph.addEdge(counter , counter + 1) ;
         }
         assertNotEquals(1 ,  Solution.computeSolution(sampleGraph , 2));
+    }
+
+
+    /**
+     * same graph for test case 3 and 4
+     * 3 vertices
+     *  0 - 1 - 2
+     *
+     *  when c = 2 , only one element should be deleted
+     *  when c = 1 ,
+     * */
+    @Test
+    public void TestTwoPositive(){
+        Graph<Integer , DefaultEdge> sampleGraph = new SimpleGraph<>(DefaultEdge.class);
+        addVertex(sampleGraph , 3);
+        for(int counter = 0 ; counter < sampleGraph.vertexSet().size() - 1 ;counter++){
+            sampleGraph.addEdge(counter , counter +1) ;
+        }
+        assertEquals(1, Solution.computeSolution(sampleGraph, 2));
+    }
+
+    /**
+     * Test case 4 -- Not working?? Figure out
+     * change c while keeping the graph same
+     * 0-1-2
+     *
+     *  */
+    @Test
+    public void TestTwoPositiveTwo(){
+        Graph<Integer , DefaultEdge> sampleGraph = new SimpleGraph<>(DefaultEdge.class);
+        addVertex(sampleGraph , 3);
+        for(int counter = 0 ; counter < sampleGraph.vertexSet().size() - 1 ;counter++){
+            sampleGraph.addEdge(counter , counter +1) ;
+        }
+
+        // should be 2
+        assertEquals(1, Solution.computeSolution(sampleGraph, 1));
     }
 
 
